130207c06df76a2b952c61961f77a1d98b0e12cb
finegs <finegs@gmail.com>
2021-03-14 09:46:23 +0900

    a1 with swap2

--- a1.cpp
+++ a1.cpp
@@ -13,22 +13,80 @@
 
 using namespace std;
 
-#define swap(a,b) {int t = a; (a) = (b); (b) = t; }
+//#define swap(a,b) {int* t = a; (a) = (b); (b) = t; }
+/*#define swap(x,y) do \
+{ unsigned char swap_temp[sizeof(x) == sizeof(y) ? sizeof(x) : -1]; \
+	memcpy(swap_temp, &y, sizeof(x)); \
+	memcpy(&y, &x, sizeof(x)); \
+	memcpy(&x, swap_temp, sizeof(x)); \
+} while(0)
+*/
+void swap2(void** pa, void** pb);
 static char timestamp_buf[25];
 const char* timestamp(char* buf = timestamp_buf);
+int comp_int(void* a, void* b);
+void heap_push(void* arr, int pos, int (*comp)(void *a, void*b));
+void heap_pop(void* arr, int n, int pos, int (*comp)(void *a, void*b));
+void print(int arr[], int arr_len);
+const char* timestamp(char* buf);
 
-void heap_push(int arr[], int pos) {
-	if(pos<=1||arr[pos/2]>arr[pos]) return;
-	swap(arr[pos/2], arr[pos]);
-	heap_push(arr, pos/2);
+void swap_void(void* a, void* b, size_t size);
+
+int main(int argc, char* argv[]) {
+
+	fprintf(stdout, "%s %s is started.\n", timestamp(), argv[0]);
+	int arr_len = 10;
+	int verbose = 0;
+	for(int i = 1;i<argc;i++) {
+		if((!strcmp("-n", argv[i]) || !strcmp("--num", argv[i])) && i+1<argc) {
+			arr_len = atoi(argv[i+1]); i++;
+		}
+		if(!strcmp("-v", argv[i]) || !strcmp("--verbose", argv[i])) {
+			verbose = 1;
+		}
+	}
+
+	if(arr_len <=0) {
+	  arr_len = 10;
+	  printf("%s Array Size (Default:%d) ", timestamp(), arr_len); fflush(stdout);
+	}
+
+	fprintf(stdout, "%s malloc(%d) is done.\n", timestamp(), arr_len+1);
+	int* arr = (int*)malloc(sizeof(int)*(arr_len+1));
+	if(!arr) { fprintf(stderr, "%s fail to malloc(%d)", timestamp(), arr_len); return EXIT_FAILURE;}
+
+	srand((unsigned int)time(NULL));
+	//std::for_each_n(arr+1, arr_len, [](int &a) { a = rand(); });	
+	fprintf(stdout, "%s srand(time(NULL)) is done.\n", timestamp());
+	for(int i = 1;i <= arr_len;i++) { arr[i] = rand(); heap_push(arr, i, comp_int); }	
+
+	fprintf(stdout, "%s arr = rand() && heap_push(%d) is done.\n", timestamp(), arr_len+1);
+	if(verbose) {print(arr, arr_len); fprintf(stdout, "\n"); }
+	for(int i = arr_len;i>1;i--) { swap2((void**)&arr+1, (void**)&arr+i); heap_pop(arr, i-1, 1, comp_int); }
+
+	fprintf(stdout, "%s swap & heap_pop(%d) is done.\n", timestamp(), arr_len+1);
+	if(verbose) {print(arr, arr_len); fprintf(stdout, "\n"); }
+
+	free(arr);
+
+	cout << "Good " << endl;
+
+	return 0;
 }
 
-void heap_pop(int arr[], int n, int pos) {
+
+void heap_push(void* arr, int pos, int (*comp)(void* a, void* b)) {
+	if(pos<=1||comp((int*)arr+pos/2, (int*)arr+pos)>0) return;
+	swap2(&arr+pos/2, &arr+pos);
+	heap_push(arr, pos/2, comp);
+}
+
+void heap_pop(void* arr, int n, int pos, int (*comp)(void* a, void* b)) {
 	int np = pos*2;
-	if(np<n&&arr[np]<arr[np+1]) np++;
-	if(np>n||arr[pos]>=arr[np]) return;
-	swap(arr[pos], arr[np]);
-	heap_pop(arr, n, np);
+	if(np<n&&comp((int*)arr+np,(int*)arr+np+1)<0) np++;
+	if(np>n||comp((int*)arr+pos,(int*)arr+np)<=0) return;
+	swap2(&arr+pos, &arr+np);
+	heap_pop(arr, n, np, comp);
 }
 
 void print(int arr[], int arr_len) {
@@ -57,44 +115,12 @@ const char* timestamp(char* buf) {
 	return buf;
 }
 
-int main(int argc, char* argv[]) {
+int comp_int(void* a, void* b) {
+	return *(int*)a - *(int*)b;
+}
 
-	fprintf(stdout, "%s %s is started.\n", timestamp(), argv[0]);
-	int arr_len = 10;
-	int verbose = 0;
-	for(int i = 1;i<argc;i++) {
-		if((!strcmp("-n", argv[i]) || !strcmp("--num", argv[i])) && i+1<argc) {
-			arr_len = atoi(argv[i+1]); i++;
-		}
-		if(!strcmp("-v", argv[i]) || !strcmp("--verbose", argv[i])) {
-			verbose = 1;
-		}
-	}
-
-	if(arr_len <=0) {
-	  arr_len = 10;
-	  printf("%s Array Size (Default:%d) ", timestamp(), arr_len); fflush(stdout);
-	}
-
-	fprintf(stdout, "%s malloc(%d) is done.\n", timestamp(), arr_len+1);
-	int* arr = (int*)malloc(sizeof(int)*(arr_len+1));
-	if(!arr) { fprintf(stderr, "%s fail to malloc(%d)", timestamp(), arr_len); return EXIT_FAILURE;}
-
-	srand((unsigned int)time(NULL));
-	//std::for_each_n(arr+1, arr_len, [](int &a) { a = rand(); });	
-	fprintf(stdout, "%s srand(time(NULL)) is done.\n", timestamp());
-	for(int i = 1;i <= arr_len;i++) { arr[i] = rand(); heap_push(arr, i);};	
-
-	fprintf(stdout, "%s arr = rand() && heap_push(%d) is done.\n", timestamp(), arr_len+1);
-	if(verbose) {print(arr, arr_len); fprintf(stdout, "\n"); }
-	for(int i = arr_len;i>1;i--) { swap(arr[1], arr[i]); heap_pop(arr, i-1, 1); }
-
-	fprintf(stdout, "%s swap & heap_pop(%d) is done.\n", timestamp(), arr_len+1);
-	if(verbose) {print(arr, arr_len); fprintf(stdout, "\n"); }
-
-	free(arr);
-
-	cout << "Good " << endl;
-
-	return 0;
+void swap2(void** pa, void** pb) {
+	void *pc = *pa;
+	*pa = *pb;
+	*pb = pc;
 }
