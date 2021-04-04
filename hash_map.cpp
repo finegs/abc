032e2d38bd23572dac0e3563e752572498a1774f
finegs <finegs@gmail.com>
2021-04-04 23:05:04 +0900

    hash_map.cpp with compile error  -  MyHash<unsigned int, const char*> not defined Link Error

--- hash_map.cpp
+++ hash_map.cpp
@@ -1,45 +1,46 @@
 #include "hash_map.hpp"
 
-template<typename TKey, typename TValue>
-MyHashMap<TKey, TValue>::MyHashMap() {
+template<typename TKey, typename TValue, size_t Bucket_len>
+MyHashMap<TKey, TValue, Bucket_len>::MyHashMap() {
 }
-template<typename TKey, typename TValue>
-int MyHashMap<TKey, TValue>::CalcHashFunc(TKey key) {
-    return key % MAX_COUNT;
+
+template<typename TKey, typename TValue, size_t Bucket_len>
+int MyHashMap<TKey, TValue, Bucket_len>::CalcHashFunc(TKey key) {
+    return key % Bucket_len;
 }
-template<typename TKey, typename TValue>
-void MyHashMap<TKey, TValue>::Insert(TKey key, TValue value) {
-    std::list<Data<TKey, TValue>*> &list = bucket[CalcHashFunc(key)];
+template<typename TKey, typename TValue, size_t Bucket_len>
+void MyHashMap<TKey, TValue, Bucket_len>::Insert(TKey key, TValue value) {
+    std::list<MyHashMap<TKey, TValue, Bucket_len>::Data*> &list = bucket[CalcHashFunc(key)];
     if (list.size() > 0) {
-        for (Data<TKey, TValue> *it : list) {
+        for (MyHashMap<TKey, TValue, Bucket_len>::Data *it : list) {
             if (it->key == key) {
                 it->value = value;
                 return;
             }
         }
     }
-    Data<TKey, TValue>* data = new Data<TKey, TValue>();
+    MyHashMap<TKey, TValue, Bucket_len>::Data* data = new MyHashMap<TKey, TValue, Bucket_len>::Data();
     data->key = key;
     data->value = value;
     list.push_back(data);
 }
-template<typename TKey, typename TValue>
-void MyHashMap<TKey, TValue>::Print() {
-    for (int i = 0; i < MAX_COUNT; i++) {
-        std::list<Data<TKey, TValue>*> list = bucket[i];
+template<typename TKey, typename TValue, size_t Bucket_len>
+void MyHashMap<TKey, TValue, Bucket_len>::Print() {
+    for (int i = 0; i < Bucket_len; i++) {
+        std::list<MyHashMap<TKey, TValue, Bucket_len>::Data*> list = bucket[i];
         if (list.size() > 0) {
-            for (Data<TKey, TValue> *it : list) {
+            for (MyHashMap<TKey, TValue, Bucket_len>::Data *it : list) {
                 std::cout << "key : " << it->key << " value : " << it->value << "\t";
             }
         }
         std::cout << std::endl;
     }
 }
-template<typename TKey, typename TValue>
-void MyHashMap<TKey, TValue>::Delete(TKey key) {
-    std::list<Data<TKey, TValue>*> &list = bucket[CalcHashFunc(key)];
+template<typename TKey, typename TValue, size_t Bucket_len>
+void MyHashMap<TKey, TValue, Bucket_len>::Delete(TKey key) {
+    std::list<MyHashMap<TKey, TValue, Bucket_len>::Data*> &list = bucket[CalcHashFunc(key)];
     if (list.size() > 0) {
-        for (Data<TKey, TValue> *it : list) {
+        for (MyHashMap<TKey, TValue, Bucket_len>::Data* it : list) {
             if (it->key == key) {
                 list.remove(it);
                 return;
--- hash_map.hpp
+++ hash_map.hpp
@@ -2,23 +2,29 @@
 #include <iostream>
 #include <list>
  
-template<typename TKey, typename TValue>
+template<typename TKey, typename TValue, size_t Bucket_len = 1000>
 class MyHashMap {
 private:
-    template<typename TTKey, typename TTValue>
-    struct Data
-    {
-        TKey key;
-        TValue value;
-    };
-    static int const MAX_COUNT = 5;
-    std::list<Data<TKey, TValue>*> bucket[MAX_COUNT];
+
+//	template<typename TTKey, typename TTValue>
+	struct Data;
+//	{
+//		TKey key;
+//		TValue value;
+//	};
+//    static int const MAX_COUNT = 5;
+//  std::list<MyHashMap<TKey, TValue>::Data*> bucket[MAX_COUNT];
+    std::list<typename MyHashMap<TKey, TValue>::Data*> bucket[Bucket_len];
     int CalcHashFunc(TKey key);
 public:
-    MyHashMap();
+    MyHashMap<TKey, TValue, Bucket_len>();
     void Insert(TKey key, TValue value);
     void Delete(TKey key);
     void Print();
 };
  
-
+template<typename TKey, typename TValue, size_t Bucket_len>
+struct MyHashMap<TKey, TValue, Bucket_len>::Data {
+	TKey key;
+	TValue value;
+};
