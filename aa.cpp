2a3a88f3e0426dc79ad3157b203e6fc58133852f
finegs <finegs@gmail.com>
2021-04-17 22:40:49 +0900

    hashmap CLI

--- aa.cpp
+++ aa.cpp
@@ -1,9 +1,28 @@
 #include <stdio.h>
 #include <malloc.h>
-#include <string.h>
+//#include <string.h>
 #include "List.h"
 #include "Util.h"
 #include "hash_map.hpp"
+
+int memset(void* p, char v, size_t len) {
+	char* c = (char*)p;
+	while(len--) {
+		*c = v;
+		c++;
+	}
+	return 0;
+}
+int strcmp(const char* a, const char* b) {
+	char* aa = (char*)a;
+	char* bb = (char*)b;
+	while(1) {
+		if(aa[0] != bb[0]) return (aa[0] > bb[0] ? 1 : -1);
+		if(!*aa) break;
+		aa++;b++;
+	}
+	return 0;
+}
 
 struct MyCharsHash {
 	unsigned long operator()(const char* str) {
@@ -17,6 +36,16 @@ struct MyCharsHash {
 		return h;
 	}
 };
+
+void printUsage() {
+	printf("Usage : cmd [ key value]\n");
+	printf("        --exit, --quit\n");
+	printf("        --add(-a) key value\n");
+	printf("        --remove(-r) key\n");
+	printf("        --get(-g) key\n");
+	printf("        --print(-p)\n");
+	printf("        --clearall(-ca)\n");
+}
 
 
 int main() {
@@ -34,28 +63,45 @@ int main() {
 
 	list.printList();
 
+	HashMap<const char*, int, MyCharsHash> map;
+
 	size_t str_len = 1024+1;
-	char str[str_len];
-	memset(str, 0, str_len);
+	char cmd[32+1], str[str_len], value[12];
 
 	while(1) {
-    	printf("Enter string to hash : "); fflush(stdout);
-    	scanf("%s", str); fflush(stdin);
-		if(!strcmp("exit", str) || !strcmp("quit", str)) break;
-    	printf("hash(%s) : %d\n", str, strhash(str));
+		memset(str, 0, str_len);
+		memset(cmd, 0, 32+1);
+		memset(value,0,12);
+
+    	printf("Enter cmd [key value] : "); fflush(stdout);
+    	scanf("%s", cmd);
+		if(!strcmp("--exit", cmd) || !strcmp("--quit", cmd)) break;
+		else if(!strcmp("--help", cmd) || !strcmp("-h", cmd)) { printUsage(); }
+		else if(!strcmp("--add",cmd) ||!strcmp("-a", cmd)) { scanf("%s %s", str, value);  map.put(str, atoi(value)); }
+		else if (!strcmp("--remove",cmd) ||!strcmp("-r", cmd)) { scanf("%s", str); map.remove(str); }
+		else if (!strcmp("--clearall",cmd) ||!strcmp("-ca", cmd)) { map.clearall(); }
+		else if (!strcmp("--get",cmd) ||!strcmp("-g", cmd)) { scanf("%s", str); map.remove(str); }
+		else if (!strcmp("--print",cmd) ||!strcmp("-p", cmd)) { map.print(); }
+		else { printUsage(); }
+		
+    	printf("\t\t\t[Debug] hash(%s) : %d\n", cmd, strhash(cmd));
+		fflush(stdin);
 	}
 
+#if 0
 	int n;
 	printf("number : "); fflush(stdout);
 	scanf("%d", &n); fflush(stdin);
 	printf("prime_max(%d) : %d\n", n, prime_max(n));
+#endif
 
-
+#if 0
 	HashMap<const char*, int, MyCharsHash> map;
 	map.put("1", 1);
 	int iv;
 	bool b = map.get("1", iv);
 	std::cout<< "map.get("<< "1" << ") = " << (b ? "true" : "false") << ", value = " << iv << std::endl;
+#endif
 
 	return 0;
 }
--- hash_map.hpp
+++ hash_map.hpp
@@ -2,6 +2,8 @@
 
 #include <iostream>
 
+#define HASH_MAP_USE_KEYSET 0
+
 template<typename K, size_t TableSize=1000000>
 struct KeyHash {
 	unsigned long operator() (const K& key) const  {
@@ -30,19 +32,40 @@ class HashMap {
 	public:
 		HashMap() {
 			table = new HashNode<Key,Value>* [TableSize];
+#if HASH_MAP_USE_KEYSET
+			keySet = nullptr;
+#endif
 		}
 
 		~HashMap() {
+#if 1
 			for (int i = 0; i < TableSize; ++i) {
-				HashNode<Key,Value> *entry = table[i];
+			 	HashNode<Key,Value> *entry = table[i];
+#endif
+#if HASH_MAP_USE_KEYSET
+			for (HashNode<Key,int> *keyItem = keySet; keyItem;keyItem=keyItem->getNext()) {
+				HashNode<Key,int> *keyItemPrev = keyItem;
+				HashNode<Key,Value> *entry = table[keyItem->getValue()];
+
+#endif
 				while(entry != NULL) {
 					HashNode<Key,Value> *pre = entry;
 					entry = entry->getNext();
 					delete pre;
 				}
+#if 1	
 				table[i] = nullptr;
+#endif
+#if HASH_MAP_USE_KEYSET
+				table[keyItem->getValue()] = nullptr;
+				delete keyItemPrev;
+#endif
 			}
 			delete[] table;
+			table = nullptr;
+#if HASH_MAP_USE_KEYSET
+			keySet = nullptr;
+#endif
 		}
 
 		bool get(const Key &key, Value &value) {
@@ -79,6 +102,19 @@ class HashMap {
 			else {
 				entry->setValue(value);
 			}
+			
+			// add newKey to keySet List
+#if HASH_MAP_USE_KEYSET
+			HashNode<Key,int>* newKey = new HashNode<Key, int>(key, hashValue);
+			if(keySet) {
+				newKey->setNext(keySet);
+				keySet = newKey;
+			}
+			else {
+				keySet = newKey;
+				keySet->setNext(nullptr);
+			}
+#endif
 		}
 
 		void remove(const Key& key) {
@@ -105,10 +141,48 @@ class HashMap {
 			}
 		}
 
+		void print() {
+
+			HashNode<Key, Value> *prev;
+			HashNode<Key, Value> *entry;
+
+			for(int i = 0;i<TableSize;i++) {
+
+				prev = nullptr;
+				entry = table[i];
+
+				if(!entry) continue;
+
+				printf("[%d] : ", i);
+
+				while(entry != nullptr) {
+					printf("(%s, %s)", entry->getKey(), entry->getValue());	
+					entry = entry->getNext();	
+					if(entry) printf(",");
+				}
+				printf("\n");
+			}
+		}
+
+		void clearall() {
+			for (int i = 0; i < TableSize; ++i) {
+			 	HashNode<Key,Value> *entry = table[i];
+
+				while(entry != NULL) {
+					HashNode<Key,Value> *pre = entry;
+					entry = entry->getNext();
+					delete pre;
+				}
+				table[i] = nullptr;
+			}
+		}
+
 	private:
 		HashNode<Key, Value>** table;
+#if HASH_MAP_USE_KEYSET
+		HashNode<Key, int>* keySet;
+#endif
 		Hash hashFunc;
-
 };
 
 
