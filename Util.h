f13f0b3e3f194579380b337a7eb655180f354b64
finegs <finegs@gmail.com>
2021-05-02 12:26:05 +0900

    remove hash.cpp

--- Util.h
+++ Util.h
@@ -6,6 +6,7 @@
 
 int  strhash(const char* str, int mod = 1024*1024);
 uint32_t hash(void* buf, size_t len, uint32_t* hash);
+unsigned int hash(const void *key);
 void mmemset(void* p, int v, size_t len);
 int prime_max(int n);
 
