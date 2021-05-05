fd8bbcce7a74700084e0ddcfb683210d2ae60e0f
finegs <finegs@gmail.com>
2021-05-06 07:13:21 +0900

    std::unique_ptr<std::string>

--- ab.cpp
+++ ab.cpp
@@ -15,12 +15,15 @@ class PDesc {
 			desc_ = std::make_unique<std::string>(desc);
 		}
 
+		const char* getName() const { return name_.get()->c_str(); }
+		const char* getDesc() const { return desc_.get()->c_str(); }
+
 	private:
 		std::unique_ptr<std::string> name_;
 		std::unique_ptr<std::string> desc_;
 };
 
 int main() {
-	PDesc pd("b", "a");
+	PDesc pd(NULL, "a");
 	return 0;
 };
