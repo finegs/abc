e4e7633dfac89e80ebc9644796a80dcff83a1774
finegs <finegs@gmail.com>
2021-08-22 17:02:02 +0900

    a.cpp only left

--- a.cpp
+++ a.cpp
@@ -1,105 +1,152 @@
 #include <iostream>
-#include <stdio.h>
-
-#include "Util.h"
-#include "hash_map.hpp"
-#include "array.hpp"
-
-class Super {
-	private:
-		int m_value;
-	public:
-		void setValue(const int& value) { m_value = value;}
-		const int& getValue() const { return m_value; }
-		virtual void someMethod();
+#include <vector>
+#include <memory>
+#include <cstdio>
+#include <fstream>
+#include <cassert>
+#include <functional>
+ 
+// helper class for runtime polymorphism demo below
+struct B {
+  virtual void bar() { std::cout << "B::bar\n"; }
+  virtual ~B() = default;
 };
-
-class Sub : public Super {
-	public:
-		Sub(int value) {
-			setValue(value);
-		}
-		void someMethod() override;
+struct D : B
+{
+    D() { std::cout << "D::D\n";  }
+    ~D() { std::cout << "D::~D\n";  }
+    void bar() override { std::cout << "D::bar\n";  }
 };
-
-void Super::someMethod() {
-	std::cout << " Super::" << "someMethod() , m_value:" << getValue() << std::endl;
+ 
+// a function consuming a unique_ptr can take it by value or by rvalue reference
+std::unique_ptr<D> pass_through(std::unique_ptr<D> p)
+{
+    p->bar();
+    return p;
 }
+ 
+// helper function for the custom deleter demo below
+void close_file(std::FILE* fp) { std::fclose(fp); }
+ 
+// unique_ptr-based linked list demo
+struct List {
+  struct Node {
+    int data;
+    std::unique_ptr<Node> next;
+    Node(int data) : data{data}, next{nullptr} {}
+  };
+  List() : head{nullptr} {};
+  // N.B. iterative destructor to avoid stack overflow on long lists
+  ~List() { clean(); }
+  // copy/move and other APIs skipped for simplicity
+  void push(int data) {
+    auto temp{std::make_unique<Node>(data)};
+    if(head) temp->next = std::move(head);
+    head = std::move(temp);
+  }
 
-void Sub::someMethod() {
-	std::cout << " Sub::" << "someMethod(), m_value:" << getValue() << std::endl;
-}
+  void pop() {
+      if(head == nullptr) return;
+      head = std::move(head->next);
+  }
 
-typedef unsigned int muint;
+  void remove(int data) {
+      List::Node* node = head.get();
+      while(node) {
+          if(node->data == data) {
+              List::Node* temp = node;
+              node = node->next.get();
+          }
+          node = node->next.get();
+      }
+  }
 
-struct UIntHasher  {
-	unsigned long operator()(const muint& key) const {
-		return key;
-	}
+  int find(int data) {
+      Node* node = head.get();
+      while(node) {
+          if(node->data == data) {
+              return 1;
+          }
+          if(node->next)
+            node = node->next.get();
+          else break;
+      }
+      return 0;
+  }
+
+  void clean() {
+      while(head) { head = std::move(head->next); }
+  }
+
+  friend std::ostream& operator<<(std::ostream& os, const List&o);
+private:
+  std::unique_ptr<Node> head;
 };
 
-struct UIntStrViewer {
-	void operator()(const muint& key, char* value) const {
-		printf("{%d, %s}", key, value);
-	}
-};
-
-struct UIntMatcher {
-	bool operator()(const muint& a, const muint& b) const {
-		return a == b;
-	}
-};
-
-void do_test_array();
-void do_test_class();
-
-int main(int argc, char* argv[]) {
-	int tc = 0;
-
-	printf("#%d : Input\n", ++tc);
-	for (int i = 1; i< argc; i++) {
-		printf("%d : hash(%s)=%zu\n", i, argv[i], strhash(argv[i], 1024*1024*1024)); 
-	}
-
-	HashMap<muint, char*, UIntStrViewer, UIntHasher, UIntMatcher> *map 
-		= new HashMap<muint, char*, UIntStrViewer, UIntHasher, UIntMatcher>;
-
-	printf("#%d : Put\n", ++tc);
-	for (int i = 1; i< argc; i++) {
-		printf("%d : hash(%s)=%zu\n", i, argv[i], strhash(argv[i], 1024*1024*1024)); 
-		map->put(strhash(argv[i], 1024*1024*1024), argv[i]);
-	}
-
-	printf("#%d : Print\n", ++tc);
-	map->print();
-
-	printf("#%d : Remove\n", ++tc);
-	for (int i = 1; i< argc; i++) {
-		map->remove(strhash(argv[i], 1024*1024*1024));
-	}
-
-	delete(map);
-
-
-	do_test_array();
-	do_test_class();
-	system("pause");
-
-	return 0;
+std::ostream& operator<<(std::ostream& os, const List& o) {
+    List::Node* head = o.head.get();
+    while(head) { os << head->data << ' '; head = head->next.get(); }
+    return os;
 }
+ 
+int main()
+{
+  std::cout << "1) Unique ownership semantics demo\n";
+  {
+      auto p = std::make_unique<D>(); // p is a unique_ptr that owns a D
+      auto q = pass_through(std::move(p)); 
+      assert(!p); // now p owns nothing and holds a null pointer
+      q->bar();   // and q owns the D object
+  } // ~D called here
+ 
+  std::cout << "2) Runtime polymorphism demo\n";
+  {
+    std::unique_ptr<B> p = std::make_unique<D>(); // p is a unique_ptr that owns a D
+                                                  // as a pointer to base
+    p->bar(); // virtual dispatch
+ 
+    std::vector<std::unique_ptr<B>> v;  // unique_ptr can be stored in a container
+    v.push_back(std::make_unique<D>());
+    v.push_back(std::move(p));
+    v.emplace_back(new D);
+    for(auto& p: v) p->bar(); // virtual dispatch
+  } // ~D called 3 times
+ 
+  std::cout << "3) Custom deleter demo\n";
+  std::ofstream("demo.txt") << 'x'; // prepare the file to read
+  {
+      std::unique_ptr<std::FILE, decltype(&close_file)> fp(std::fopen("demo.txt", "r"),
+                                                           &close_file);
+      if(fp) // fopen could have failed; in which case fp holds a null pointer
+        std::cout << (char)std::fgetc(fp.get()) << '\n';
+  } // fclose() called here, but only if FILE* is not a null pointer
+    // (that is, if fopen succeeded)
+ 
+  std::cout << "4) Custom lambda-expression deleter demo\n";
+  {
+    std::unique_ptr<D, std::function<void(D*)>> p(new D, [](D* ptr)
+        {
+            std::cout << "destroying from a custom deleter...\n";
+            delete ptr;
+        });  // p owns D
+    p->bar();
+  } // the lambda above is called and D is destroyed
+ 
+  std::cout << "5) Array form of unique_ptr demo\n";
+  {
+      std::unique_ptr<D[]> p(new D[3]);
+  } // calls ~D 3 times
+ 
+  std::cout << "6) Linked list demo\n";
+  {
+    List l;
+    for(long n = 1; n != 1'000'000; ++n) l.push(n);
 
-void do_test_array() {
+    std::cout << "l.find(" << 999999 << ")=" << (l.find(999999) ? "true" : "false") << std::endl;
+    l.pop();
+    std::cout << "l.find(" << 999999 << ")=" << (l.find(999999) ? "true" : "false") << std::endl;
 
-	int arr[3] = {1, 2,100};
-
-	Array<int, 3> arr2(arr);
-
-	arr2.print();
-
-}
-
-void do_test_class() {
-	Sub sb{100};
-	sb.someMethod();
-
+    std::cout << "destroying 1'000'000 nodes... ";
+  } // destroys all 1 million nodes
+  std::cout << "Done.\n";
 }
