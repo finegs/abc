7b898e6972d05a90cea262aa1f5e8bdf91502eea
finegs <finegs@gmail.com>
2021-10-29 18:38:40 +0900

    std::unorderd_map with struct (std::hash<Location>)

--- a.cpp
+++ a.cpp
@@ -1,3 +1,4 @@
+#if 0
 #include <iostream>
 #include <stdio.h>
 
@@ -23,4 +24,50 @@ int main(int argc, char* argv[]) {
 	delete(map);
 
 	return 0;
+}
+#endif
+
+#include <iostream>
+#include <unordered_map>
+
+using namespace std;
+
+struct Location {
+	int row;
+	int col;
+
+	bool operator==(const Location& o) const noexcept {
+		return row == o.row && col == o.col;
+	}
+
+	std::size_t hash() const noexcept {
+		return (row<1) ^ col;
+	}
+};
+
+namespace std 
+{
+	template<>
+	struct hash<Location> {
+		std::size_t operator()(const Location& o) const noexcept {
+			return (o.row < 1) ^ o.col;
+		}
+	};
+}
+
+int main() {
+	unordered_map<Location, const char*> m;
+	m.insert({{1,1}, "1_1"});
+	m.insert({{1,3}, "1_1"});
+	m.insert({{1,2}, "1_1"});
+	m.insert({{5,1}, "1_1"});
+
+	cout << "m.at[{" << 1 << "," << 1 << "}]=" << m.at({1,1}) << std::endl;
+	cout << "m[{" << 1 << "," << 2 << "}]=" << m[{1,2}] << std::endl;
+
+	for(auto i : m) {
+		//cout << "m.at[{" << i.first.row << ", " << i.first.col << "}]=" << m.at({i.first.row, i.first.col}) << std::endl;
+		cout << "m.at[{" << i.first.row << ", " << i.first.col << "}]=" << m.at({i.first.row, i.first.col}) << std::endl;
+	}
+
 }
