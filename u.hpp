2b1a19160a4ecd3d543b7d0b860075c708644fb9
finegs <finegs@gmail.com>
2021-12-31 13:04:22 +0900

    u.hpp

--- u.hpp
+++ u.hpp
@@ -1,53 +1,39 @@
-
 #pragma once
 
 #include <cstdio>
-#include <sys/time.h>
-#include <sys/timeb.h>
-#include <time.h>
+#include <ctime>
 #include <cmath>
+#include <sys/time.h>
 
-#ifdef __cplusplus__
-extern "C" {}
+#if defined(__cplusplus)
+extern "C" {
 #endif
 
-const char* tmstr(char str[25]);
-#define TIME_UTC time(NULL)
-inline const char* tmstr(char str[25]) {
-
-    //time_t tm;
-    //struct timespec ts;
-    struct tm ti;
-	struct timeval tv;
-	time_t t;
-
-	gettimeofday(&tv, NULL);
-
-    //tm = time(NULL);
-//    timespec_get(&ts, (int)TIME_UTC);
-    //ti = localtime(&tm);
-	t  = tv.tv_sec;
-    localtime_s(&ti, &t);
-    
-    strftime(str, 25, "%Y-%m-%d %H:%M:%S", &ti);
-    sprintf(str, "%s.%03ld", str, lround(tv.tv_usec/1e3));
-	return str;
-}
-
-
-#ifndef NDEBUG
-#define DBG_LOG(msg) do { std::cout << tmstr(__tmstr) << " (" << __FILE__ << ":" << __LINE__ << "): " << msg << '\n'; } while(false)
-#else
-#define DEB_LOG(msg) do {} while {false}
-#endif
-
-long millis(){
+inline long curTimeMillis()
+{
     struct timespec _t;
     clock_gettime(CLOCK_REALTIME, &_t);
-    return _t.tv_sec*1000 + lround(_t.tv_nsec/1e6);
+    return _t.tv_sec * 1000 + lround(_t.tv_nsec / 1e6);
 }
 
+inline long curTimeMillis_s(struct timespec *ts)
+{
+    clock_gettime(CLOCK_REALTIME, ts);
+    return ts->tv_sec * 1000 + lround(ts->tv_nsec / 1e6);
+}
 
-#ifdef __cplusplus__
-extern }
+inline const char* tmstr(char* tstr) {
+	struct timespec ts; 
+	struct tm ti;
+
+	localtime_r((time_t*)&ts.tv_sec, &ti);
+
+	strftime(tstr, 25, "%Y-%m-%d %H:%M:%S", &ti);
+	sprintf(tstr, "%s.%03d", tstr, (int)lround(ts.tv_nsec/1e6));
+	return tstr;
+}
+
+#if defined(__cplusplus)
+}
 #endif
+
