9c0deb205bc0b78088d4b59d134aaa65e50df6bd
finegs <finegs@gmail.com>
2022-01-03 18:39:54 +0900

    msvc,mingw,linux

--- u.hpp
+++ u.hpp
@@ -3,86 +3,102 @@
 #include <cstdio>
 #include <ctime>
 #include <cmath>
+#if defined(__MSC_VER)
+#include <windows.h>
+#elif defined(__MINGW32__) || defined(__MINGW64__)
+#include <sys/timeb.h>
+#elif defined(__linux__)
 #include <sys/time.h>
+#endif
 
 #if defined(__cplusplus)
 extern "C" {
 #endif
 
-<<<<<<< HEAD
-inline long curTimeMillis()
+
+#ifndef NDEBUG
+#define DBG_LOG(msg) do { std::cout << tmstr(__tmstr) << ":" << __FILE__ << ":" << __LINE__ << "): " << msg << '\n'; } while(false)
+#else
+#define DEB_LOG(msg) do {} while {false}
+#endif
+
+inline uint64_t curTimeMillis()
 {
+#if defined (__linux__)
     struct timespec _t;
     clock_gettime(CLOCK_REALTIME, &_t);
     return _t.tv_sec * 1000 + lround(_t.tv_nsec / 1e6);
+
+#elif defined(_MINGW32__) || defined(__MINGW64__)
+    struct _timeb timebuffer;
+    _ftime(&timebuffer);
+    return (uint64_t)(((timebuffer.time * 1000) + timebuffer.millitm));
+#else
+    struct timeb timebuffer;
+    ftime(&timebuffer);
+    return (uint64_t)(((timebuffer.time * 1000) + timebuffer.millitm));
+#endif
 }
-=======
-//const char* tmstr(char str[25]);
-//#define TIME_UTC time(NULL)
-//inline const char* tmstr(char str[25]) {
-//
-//    //time_t tm;
-//    //struct timespec ts;
-//    struct tm ti;
-//	struct timeval tv;
-//	time_t t;
-//
-//	gettimeofday(&tv, NULL);
-//
-//    //tm = time(NULL);
-////    timespec_get(&ts, (int)TIME_UTC);
-//    //ti = localtime(&tm);
-//	t  = tv.tv_sec;
-//    localtime_s(&ti, &t);
-//    
-//    strftime(str, 25, "%Y-%m-%d %H:%M:%S", &ti);
-//    sprintf(str, "%s.%03ld", str, lround(tv.tv_usec/1e3));
-//	return str;
-//}
->>>>>>> 73f0e0f5734faf98792c4f4a451b99d7a1a8a913
 
 inline long curTimeMillis_s(struct timespec *ts)
 {
+#if defined (__linux__)
     clock_gettime(CLOCK_REALTIME, ts);
     return ts->tv_sec * 1000 + lround(ts->tv_nsec / 1e6);
+#elif defined(_MINGW32__) || defined(__MINGW64__)
+    struct _timeb timebuffer;
+    _ftime(&timebuffer);
+    return (uint64_t)(((timebuffer.time * 1000) + timebuffer.millitm));
+#else
+    struct timeb timebuffer;
+    ftime(&timebuffer);
+    return (uint64_t)(((timebuffer.time * 1000) + timebuffer.millitm));
+#endif
 }
 
 inline const char* tmstr(char* tstr) {
+#if defined (__linux__)
 	struct timespec ts; 
 	struct tm ti;
 
+	clock_gettime(CLOCK_REALTIME, &ts);
 	localtime_r((time_t*)&ts.tv_sec, &ti);
-
-<<<<<<< HEAD
 	strftime(tstr, 25, "%Y-%m-%d %H:%M:%S", &ti);
 	sprintf(tstr, "%s.%03d", tstr, (int)lround(ts.tv_nsec/1e6));
 	return tstr;
+#elif defined (__MINGW32__ ) || defined(__MINGW64__)
+
+	struct timespec ts;
+	clock_gettime(CLOCK_REALTIME, &ts);
+	//localtime_r((time_t*)&ts.tv_sec, &ti);
+	//time_t tt = time(NULL);
+	struct tm ti;
+	errno_t er;
+
+	er =  localtime_s(&ti, &ts.tv_sec);
+	strftime(tstr, 25, "%Y-%m-%d %H:%M:%S", &ti);
+	sprintf(tstr, "%s.%03d", tstr, (int)lround(ts.tv_nsec/1e6));
+	return tstr;
+#if 0
+	struct timeb tb;
+	struct tm ti;
+	ftime(&tb);
+	localtime_s(&ti, &tb.time);
+	strftime(tstr, 23, "%Y-%m-%d %H:%M:%S", &ti);
+	sprintf(tstr, "%s.%03d", tstr, tb.millitm);
+	return tstr;
+#endif
+
+#else
+	SYSTEMTIME st;
+	GetLocalTime(&st);
+	sprintf_s(tstr, "%4u:%02u:%02u %02u:%02u:%02u:%03u",
+			st.wYear, st.wMonth, st.day, st.wHour, st.wMinute, st.wSecond, st.wMilliseconds);
+	return tstr;
+#endif
+
 }
-=======
-inline long curTimeMillis()                                                                                           1 {
-    struct timespec _t;
-    clock_gettime(CLOCK_REALTIME, &_t);
-    return _t.tv_sec * 1000 + lround(_t.tv_nsec / 1e6);
-}
 
-inline long curTimeMillis_s(struct timespec *ts)
-{
-    clock_gettime(CLOCK_REALTIME, ts);
-    return ts->tv_sec * 1000 + lround(ts->tv_nsec / 1e6);
-}
-
-inline const char* tmstr(char tstr[25]) {
-    struct timespec ts;
-    struct tm ti;
-
-    localtime_r((time_t*)&ts.tv_sec, &ti);
-
-    strftime(tstr, 25, "%Y-%m-%d %H:%M:%S", &ti);
-    sprintf(tstr, "%s.%03d", tstr, (int)lround(ts.tv_nsec/1e6));
-    return tstr;
-}
-
->>>>>>> 73f0e0f5734faf98792c4f4a451b99d7a1a8a913
 
 #if defined(__cplusplus)
 }
