192d6589e29251e0bab59aa1820cc008bb4852e7
finegs <finegs@gmail.com>
2024-07-14 11:28:17 +0900

    ..

--- cppapp/a.cpp
+++ cppapp/a.cpp
@@ -13,8 +13,8 @@ struct Log {
   int id, x1, x2, y;
 
   bool operator<(const Log& o) const {
-    if (x2 == o.x2) return x1 < o.x1;
-    return x2 > o.x2; 
+    if (x1 == o.x1) return x2 < o.x2;
+    return x1 > o.x1; 
   }
 
   friend std::ostream& operator<<(std::ostream& os, const Log& o) {
@@ -47,17 +47,17 @@ int main(int argc, char *argv[])
   cin >> N >> K;
 
   unordered_map<int, Log> logs;
-  vector<Log> logss(1000010);
+  vector<Log> logss;
   Log log;
-  logss.resize(100010);
   for (int i = 0 ; i < N;i++) {
       cin >> log.id >> log.x1>> log.x2>> log.y; 
-      logss[i] = log;
+      logss.push_back(log);
       logs.insert({log.id, log});
   }
 
-  char sep[]{'\0', ' ', '\0'};
-  std::cout << logss << endl;
+  cout << "#1 : " << logss << endl;
+  sort(logss.begin(), logss.end());
+  cout << "#2 : " << logss << endl;
   return 0;
 }
 
