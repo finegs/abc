34ef4cf74004a65033281859156bbe66eea06472
finegs <finegs@gmail.com>
2024-07-19 09:05:55 +0900

    a.cpp

--- cppapp/a.cpp
+++ cppapp/a.cpp
@@ -6,59 +6,16 @@
 #include <unordered_map>
 #include <vector>
 #include <algorithm>
+#include <cstdio>
 
 using namespace std;
 
-struct Log {
-  int id, x1, x2, y;
-
-  bool operator<(const Log& o) const {
-    if (x1 == o.x1) return x2 > o.x2;
-    return x1 < o.x1; 
-  }
-
-  friend std::ostream& operator<<(std::ostream& os, const Log& o) {
-    os << "{";
-    os << "id:" << o.id;
-    os << "," << "x1:" << o.x1;
-    os << "," << "x2:" << o.x2;
-    os << "," << "y:" << o.y;
-    os << "}";
-    return os;
-  }
-}; 
-
-template<typename T>
-std::ostream& operator<<(std::ostream& os, std::vector<T>& v) {
-  char sep[3]{'\0', ',', '\0'};
-  os << "len : " << v.size();
-  os << "," << "contents : [";
-  for (const auto& o: v) {
-    os << sep << o;
-    sep[0]=',';
-  }
-  os << "]";
-  return os;
-}
-
-int main(int argc, char *argv[])
+int main()
 {
-  int N,K;
-  cin >> N >> K;
-
-  unordered_map<int, Log> logs;
-  vector<Log> logss;
-  Log log;
-  for (int i = 0 ; i < N;i++) {
-      cin >> log.id >> log.x1>> log.x2>> log.y; 
-      logss.push_back(log);
-      logs.insert({log.id, log});
-  }
-
-  cout << "#1 : " << logss << endl;
-  sort(logss.begin(), logss.end());
-  cout << "#2 : " << logss << endl;
-  return 0;
+    std::ios::sync_with_stdio();
+    std::cout << "a\n";
+    std::printf("b\n");
+    std::cout << "c\n";
 }
 
 #endif
