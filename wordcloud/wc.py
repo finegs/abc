888cf2690f97fa16673b9b6c6117a5f4ba1757aa
finegs <finegs@gmail.com>
2025-07-11 20:42:20 +0900

    abc - algo, wordcloud : not yet

--- wordcloud/wc.py
+++ wordcloud/wc.py
@@ -1,3 +1,36 @@
+from wordcloud import WordCloud, STOPWORDS
+from matplotlib import font_manager, rc
+import matplotlib.pyplot as plt
+import numpy as np
+from PIL import Image
+import platform
+
+
+path = "/usr/local/share/fonts/malgun.ttf"
+wine_mask = np.array(Image.open("/home/user01/dev/git/abc/wordcloud/wine_mask.png"))
+font_name = font_manager.FontProperties(fname=path).get_name()
+
+# text = open("/home/user01/dev/git/abc/wordcloud/dict.csv").read()
+text = open("/home/user01/dev/git/abc/wordcloud/words.txt").read()
+
+stopwords = set(STOPWORDS)
+stopwords.add("unto")
+stopwords.add("ye")
+
+
+wc = WordCloud(
+    background_color="white", max_words=2000, mask=wine_mask, stopwords=stopwords
+)
+wc = wc.generate(text)
+wc.words_
+
+rc("font", family=font_name)
+plt.figure(figsize=(8, 8))
+plt.imshow(wine_mask, interpolation="bilinear")
+plt.axis("off")
+plt.show()
+
+'''
 from wordcloud import WordCloud
 import numpy as np
 from PIL import Image
@@ -6,51 +39,62 @@ import matplotlib.pyplot as plt
 
 from wordcloud import get_single_color_func
 
+
 class GroupedColorFunc(object):
-	def __init__(self, color_to_words, default_color):
-		self.color_func_to_words = [
-			(get_single_color_func(color), set(words))
-			for (color, words) in color_to_words.items()]
+    def __init__(self, color_to_words, default_color):
+        self.color_func_to_words = [
+            (get_single_color_func(color), set(words))
+            for (color, words) in color_to_words.items()
+        ]
 
-		self.default_color_func = get_single_color_func(default_color)
+        self.default_color_func = get_single_color_func(default_color)
 
-	def get_color_func(self, word):
-		"""Returns a single_color_func associated with the word"""
-		try:
-			color_func = next(
-				color_func for (color_func, words) in self.color_func_to_words
-				if word in words)
-		except StopIteration:
-			color_func = self.default_color_func
+    def get_color_func(self, word):
+        """Returns a single_color_func associated with the word"""
+        try:
+            color_func = next(
+                color_func
+                for (color_func, words) in self.color_func_to_words
+                if word in words
+            )
+        except StopIteration:
+            color_func = self.default_color_func
 
-		return color_func
+        return color_func
 
-	def __call__(self, word, **kwargs):
-		return self.get_color_func(word)(word, **kwargs)
+    def __call__(self, word, **kwargs):
+        return self.get_color_func(word)(word, **kwargs)
+
 
 class SimpleGroupedColorFunc(object):
-	def __init__(self, color_to_words, default_color):
-		self.word_to_color = {word: color
-							  for (color, words) in color_to_words.items()
-							  for word in words}
+    def __init__(self, color_to_words, default_color):
+        self.word_to_color = {
+            word: color for (color, words) in color_to_words.items() for word in words
+        }
 
-		self.default_color = default_color
+        self.default_color = default_color
 
-	def __call__(self, word, **kwargs):
-		return self.word_to_color.get(word, self.default_color)
+    def __call__(self, word, **kwargs):
+        return self.word_to_color.get(word, self.default_color)
 
-alice_mask = np.array(Image.open("alice_mask.png"))
 
-font_path = 'C:/Windows/fonts/h2gtre.ttf'
+# alice_mask = np.array(Image.open("/home/user01/dev/git/abc/wordcloud/alice_mask.png"))
+alice_mask = np.array(Image.open("/home/user01/dev/git/abc/wordcloud/wine_mask.png"))
+
+# font_path = "/mnt/c/Windows/fonts/h2gtre.ttf"
+font_path = "/mnt/c/Windows/fonts/malgun.ttf"
+text = open("/home/user01/dev/git/abc/wordcloud/../dict.csv").read()
 
 wordcloud = WordCloud(
-	font_path = font_path,
-	width = 800,
-	height = 800,
-	background_color="white",
-	mask = alice_mask
+    font_path=font_path,
+    width=800,
+    height=800,
+    background_color="",
+    mask=alice_mask,
 )
 
+color_to_words = wordcloud.generate(text)
+default_color = wordcloud.to_array()
 
 grouped_color_func = GroupedColorFunc(color_to_words, default_color)
 wordcloud.recolor(color_func=grouped_color_func)
@@ -59,9 +103,11 @@ plt.imshow(wordcloud, interpolation="bilinear")
 plt.axis("off")
 plt.show()
 
-imple_color_func = SimpleGroupedColorFunc(color_to_words, default_color)
+simple_color_func = SimpleGroupedColorFunc(color_to_words, default_color)
 wordcloud.recolor(color_func=simple_color_func)
 plt.figure(figsize=(10, 10))
 plt.imshow(wordcloud, interpolation="bilinear")
 plt.axis("off")
 plt.show()
+
+'''
--- wordcloud/wc01.py
+++ wordcloud/wc01.py
@@ -1,18 +1,15 @@
-
 from wordcloud import WordCloud
-#from wordcloud import STOPWORDS
+# from wordcloud import STOPWORDS
 
-texts = ['이것 은 예문 입니다', '여러분 의 문장을 넣 으세요']
-keywords = {'이것':5, '예문':3, '단어':5, '빈도수':3}
+texts = "이것 은 예문 입니다 여러분 의 문장을 넣 으세요"
+keywords = {"이것": 5, "예문": 3, "단어": 5, "빈도수": 3}
 
-wordcloud = WordCloud(
-    font_path = font_path,
-    width = 800,
-    height = 800
-)
+font_path = "/usr/local/share/fonts/malgun.ttf"
+
+wordcloud = WordCloud(font_path=font_path, width=800, height=800)
 wordcloud = wordcloud.generate_from_text(texts)
 wordcloud = wordcloud.generate_from_frequencies(keywords)
 
-stopwords = {'은', '입니다'}
+stopwords = {"은", "입니다"}
 
 wordcloud = WordCloud(stopwords=stopwords)
