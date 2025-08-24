6fbf3c805f16b0cfb195f53b8251c40491b6281e
finegs <finegs@gmail.com>
2025-08-24 13:38:11 +0900

    cppapp : add_test for ctest with gtest

--- cppapp/tests/my_test_file.cpp
+++ cppapp/tests/my_test_file.cpp
@@ -3,11 +3,19 @@
 // Function to be tested
 int add(int a, int b) { return a + b; }
 
+int minus(int a, int b) { return a - b; }
+
 // Test case for the add function
 TEST(AdderTest, BasicAddition) {
   EXPECT_EQ(add(2, 3), 5);
   EXPECT_EQ(add(-1, 1), 0);
   EXPECT_EQ(add(0, 0), 0);
+}
+
+TEST(MinusTest, BasicMinus) {
+  EXPECT_GE(minus(2, 1), 1);
+  EXPECT_GE(minus(2, 2), 0);
+  EXPECT_GE(minus(2, -2), 4);
 }
 
 // Main function to run tests
